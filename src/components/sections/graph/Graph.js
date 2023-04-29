c1bad132df2d602beee1e488a06c3dbf512bdb92
kedusi <kedusi@gmail.com>
2023-04-29 16:19:20 -0500

    working graph; some styling

--- src/components/sections/graph/Graph.js
+++ src/components/sections/graph/Graph.js
@@ -1,11 +1,55 @@
-import StyledDiv from './Graph.styled'
+import StyledDiv from "./Graph.styled";
 
-import { default as data } from '../../../data/Data'
+import { default as data } from "../../../data/Data";
+import CytoscapeComponent from "react-cytoscapejs";
+import { useEffect } from "react";
 
-console.log(data)
+console.log(data);
 
 export default function Graph() {
-    return <StyledDiv>
-        hello, cytoscape
+  let cyRef;
+
+  const addToGroup = (el) => el.addClass("isGrouped");
+
+  useEffect(() => {
+    console.log(cyRef);
+    cyRef.on("tap", "node", (e) => {
+      addToGroup(e.target);
+    });
+  });
+
+  return (
+    <StyledDiv>
+      <CytoscapeComponent
+        cy={(cy) => (cyRef = cy)}
+        layout={{ name: "circle" }}
+        stylesheet={[
+          {
+            selector: "node",
+            style: {
+              "background-color": "tomato",
+              label: "data(id)",
+            },
+          },
+          {
+            selector: "node[?isMutated]",
+            style: {
+              shape: "star",
+            },
+          },
+          {
+            selector: ".isGrouped",
+            style: {
+              "background-color": "green",
+            },
+          },
+        ]}
+        elements={data}
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+      />
     </StyledDiv>
+  );
 }
--- src/data/Data.js
+++ src/data/Data.js
@@ -1,22 +1,20 @@
-var elements= [
-    {data:{id:'a'}},
-    {data:{id:'b'}}
-]
-
+var elements = [{ data: { id: "a" } }, { data: { id: "b" } }];
 
 for (var i = 0; i < 10; i++) {
-    elements.push({
-        data: { id: 'node' + i }
-    }
-    );
-    var source = 'node' + i;
-    elements.push({
-        data: {
-            id: 'edge' + i,
-            source: source,
-            target: (i % 2 === 0 ? 'a' : 'b')
-        }
-    });
+  elements.push({
+    data: {
+      id: "node" + i,
+      isMutated: Math.random() > 0.5 ? true : false,
+    },
+  });
+  var source = "node" + i;
+  elements.push({
+    data: {
+      id: "edge" + i,
+      source: source,
+      target: i % 2 === 0 ? "a" : "b",
+    },
+  });
 }
 
-export default elements
+export default elements;
